5cf3b75eb899cbf2c376f429e1a1716e488c7596
Cosmin Seceleanu <8344031+cosminseceleanu@users.noreply.github.com>
2016-11-20 13:24:14 +0200

    clea code

--- src/main/java/com/cartracking/main/security/filter/CorsFilter.java
+++ src/main/java/com/cartracking/main/security/filter/CorsFilter.java
@@ -16,17 +16,11 @@ public class CorsFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
-        String token = request.getHeader("authorization");
-        if (request.getRequestURI().contains("ws")) {
-            System.out.println("skip for ws");
-            chain.doFilter(req, res);
-            return;
-        }
         response.setHeader("Access-Control-Allow-Origin", "http://localhost:9000");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, accept, Content-Type");
         response.setHeader("Access-Control-Max-Age", "3600");
-        if (request.getMethod() != "OPTIONS") {
+        if (!request.getMethod().equals("OPTIONS") || request.getRequestURI().contains("ws")) {
             chain.doFilter(req, res);
         }
     }
